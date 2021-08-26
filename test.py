eabe2f9676fdf7bea23e689a0b44ef363ebcb10a
Dhaval Soneji <dhaval@soneji.xyz>
2021-08-26 01:06:15 +0100

    Add more logging

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ from main import *
 This script imports all the methods from `main.py` and tests them individually. 
 '''
 
+print("Setting up client...")
 client = setup_client()
+print("Client setup successfully")
+
+print("Getting data...")
 data = get_data()
+print("Data retrieved successfully")
+
+
+print("Adding test user to subscriber base...")
 
 email = "test2@soneji.xyz"
 subscriber_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
@@ -17,3 +25,5 @@ try:
 except ApiClientError as error:
     print("Error: {}".format(error.text))
     raise "rethrowing the error again lol"
+
+print("User added successfully")
