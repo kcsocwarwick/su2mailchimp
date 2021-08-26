92b6f0c5fc7dec837c7e5b9694dae6d70d9874af
Dhaval Soneji <dhaval@soneji.xyz>
2021-08-26 01:03:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,13 @@ This script imports all the methods from `main.py` and tests them individually.
 client = setup_client()
 data = get_data()
 
-email = "test@soneji.xyz"
+email = "test2@soneji.xyz"
 subscriber_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
 name = "Test"
 
-add_to_mc(client, name, name, email, subscriber_hash)
+try:
+    response = add_to_mc(client, name, name, email, subscriber_hash)
+    print("Success:", response['id'], response['email_address'], "is", response['status'])
+except ApiClientError as error:
+    print("Error: {}".format(error.text))
+    raise "rethrowing the error again lol"
